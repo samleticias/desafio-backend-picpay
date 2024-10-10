e72480fc4051c0ed48ed2a95ba062a68a89d8a78
samleticias <sammyaleticia09@gmail.com>
2024-10-10 20:56:25 -0300

    first commit

---
+++ src/test/java/com/example/desafiopicpay/DesafiopicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
