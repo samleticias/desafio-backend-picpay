2b67d4538fc43eb3f8ca2b1d3aede7e415f1a0df
samleticias <sammyaleticia09@gmail.com>
2024-10-10 23:18:25 -0300

    feat: add GetMapping endpoint for listing all users

--- src/main/java/com/example/desafiopicpay/rest/controllers/UserController.java
+++ src/main/java/com/example/desafiopicpay/rest/controllers/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,4 +22,9 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAll();
+        return ResponseEntity.ok(users);
+    }
 }
