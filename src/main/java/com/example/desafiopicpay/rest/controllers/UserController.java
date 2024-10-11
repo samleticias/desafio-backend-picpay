8f12fe5e34690a39f9a99b1c8e0798f181263b2e
samleticias <sammyaleticia09@gmail.com>
2024-10-10 23:15:54 -0300

    feat: implement PostMapping method to create new users

---
+++ src/main/java/com/example/desafiopicpay/rest/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.example.desafiopicpay.rest.controllers;
+
+import com.example.desafiopicpay.domain.entities.User;
+import com.example.desafiopicpay.rest.dtos.UserDTO;
+import com.example.desafiopicpay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
+        User user = userService.insertUser(userDTO);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+}
